105ff01b32e0b6066ded6bb38eb434fb2a6566a5
CodeYang <10825197+kapaiya@user.noreply.gitee.com>
2024-02-04 01:36:16 +0800

    HI commit github6

--- crm/src/main/java/com/hspedu/HI.java
+++ crm/src/main/java/com/hspedu/HI.java
@@ -14,5 +14,6 @@ public class HI {
         System.out.println("HI,getee3");
         System.out.println("HI,getee4");
         System.out.println("HI,gethub5");
+        System.out.println("HI,gethub6");
     }
 }
