26967722a988bae874a41b305ca944c6f214f28e
CodeYang <10825197+kapaiya@user.noreply.gitee.com>
2024-02-04 01:28:47 +0800

    HI commit 03gitee

--- crm/src/main/java/com/hspedu/HI.java
+++ crm/src/main/java/com/hspedu/HI.java
@@ -9,5 +9,6 @@ public class HI {
     public static void main(String[] args) {
         System.out.println("HI!");
         System.out.println("HI!");
+        System.out.println("HI,getee");
     }
 }
