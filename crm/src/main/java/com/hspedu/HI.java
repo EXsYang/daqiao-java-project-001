5cecd2a5b54cadf79f2a93e3020993747ae92848
CodeYang <10825197+kapaiya@user.noreply.gitee.com>
2024-02-04 00:54:44 +0800

    HI commit 01

---
+++ crm/src/main/java/com/hspedu/HI.java
@@ -0,0 +1,12 @@
+package com.hspedu;
+
+/**
+ * @author yangda
+ * @create 2024-02-04-0:53
+ * @description:
+ */
+public class HI {
+    public static void main(String[] args) {
+        System.out.println("HI!");
+    }
+}
